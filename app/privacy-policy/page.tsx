d4b2ac8fb8f2a6fac53e906032aa4888eccb1ff7
RACHIDWEBDEV <rasheedolamide2015@gmail.com>
2025-07-10 17:46:31 +0100

    Privacy and policy

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -12,13 +12,13 @@ const Page = () => {
         </div>
         <div className="col-span-2">
           <p className="text-3xl lg:text-5xl font-medium mb-4">
-            This Privacy Policy applies to Linearsend Inc product ("Revve) and
-            sets out how Revve collects, uses, stores, shares and protects any
-            information that you give Revve when you subscribe to its services.
-            Revve is committed to ensuring that your privacy is protected.
-            Should we ask you to provide certain information when subscribing to
-            our services; you can be assured that it will only be used in
-            accordance with this Privacy Policy.
+            This Privacy Policy applies to Linearsend Inc product (&ldquo;Revve)
+            and sets out how Revve collects, uses, stores, shares and protects
+            any information that you give Revve when you subscribe to its
+            services. Revve is committed to ensuring that your privacy is
+            protected. Should we ask you to provide certain information when
+            subscribing to our services; you can be assured that it will only be
+            used in accordance with this Privacy Policy.
           </p>
         </div>
       </div>
