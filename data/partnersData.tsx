b05a4ad477ce9322ca65da24812ca7373322964f
RACHIDWEBDEV <rasheedolamide2015@gmail.com>
2025-07-21 12:41:06 +0100

    Added parters section

---
+++ data/partnersData.tsx
@@ -0,0 +1,32 @@
+type partnersDataType = {
+  logo: string;
+};
+
+const partnersData: partnersDataType[] = [
+  {
+    logo: "/images/partner/partner-1.svg",
+  },
+  {
+    logo: "/images/partner/partner-2.svg",
+  },
+  {
+    logo: "/images/partner/partner-3.svg",
+  },
+  {
+    logo: "/images/partner/partner-4.svg",
+  },
+  {
+    logo: "/images/partner/partner-5.svg",
+  },
+  {
+    logo: "/images/partner/partner-6.svg",
+  },
+  {
+    logo: "/images/partner/partner-7.svg",
+  },
+  {
+    logo: "/images/partner/partner-8.svg",
+  },
+];
+
+export default partnersData;
